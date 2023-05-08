3ec9d8c085941abc098c7315b477d61bcc748dfd
Caio Marinho <caio.barbosa@virtus.ufcg.edu.br>
2023-05-08 19:12:36 -0300

    update conn db

--- backend/settings.py
+++ backend/settings.py
@@ -102,17 +102,17 @@ DATABASES = {
 
 NAME_DB = os.getenv('DB_NAME', 'db.sqlite3')
 
-if 'sqlite' in NAME_DB:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
-        }
-    }
-else:
-    db_from_env = dj_database_url.config(default='postgresql://postgres:postgres@localhost:5432/{}'.format(NAME_DB),
-                                         conn_max_age=600)
-    DATABASES['default'].update(db_from_env)
+# if 'sqlite' in NAME_DB:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+#         }
+#     }
+# else:
+db_from_env = dj_database_url.config(default='postgresql://postgres:postgres@localhost:5432/{}'.format(NAME_DB),
+                                     conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
